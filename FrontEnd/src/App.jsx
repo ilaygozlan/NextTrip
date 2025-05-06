14102c48bb0e5ba67f0b0994255d28165658f835
ilaygozlan <ilaygozlan@icloud.com>
2025-05-06 14:50:32 +0300

    ss

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 import styled from 'styled-components';
 import Navbar from './components/Navbar';
@@ -7,9 +7,6 @@ import Home from './pages/Home';
 import MyTrips from './pages/MyTrips';
 import RateCountry from './pages/RateCountry';
 import CountryPage from './pages/CountryPage';
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -42,57 +39,17 @@ const MainContent = styled.main`
   padding: 2rem 0;
 `;
 
-// Protected Route component
-const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
-
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    localStorage.getItem('isAuthenticated') === 'true'
-  );
-
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-    localStorage.setItem('isAuthenticated', 'true');
-  };
-
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-    localStorage.removeItem('isAuthenticated');
-  };
-
   return (
     <Router>
       <GlobalStyle />
-      {isAuthenticated && <Navbar onLogout={handleLogout} />}
+      <Navbar />
       <MainContent>
         <Routes>
-          <Route path="/" element={
-            isAuthenticated ? <Home /> : <LandingPage />
-          } />
-          <Route path="/login" element={
-            isAuthenticated ? <Navigate to="/" /> : <Login onLogin={handleLogin} />
-          } />
-          <Route path="/signup" element={
-            isAuthenticated ? <Navigate to="/" /> : <Signup onLogin={handleLogin} />
-          } />
-          <Route path="/my-trips" element={
-            <ProtectedRoute>
-              <MyTrips />
-            </ProtectedRoute>
-          } />
-          <Route path="/rate-country" element={
-            <ProtectedRoute>
-              <RateCountry />
-            </ProtectedRoute>
-          } />
-          <Route path="/country/:countryName" element={
-            <ProtectedRoute>
-              <CountryPage />
-            </ProtectedRoute>
-          } />
+          <Route path="/" element={<Home />} />
+          <Route path="/my-trips" element={<MyTrips />} />
+          <Route path="/rate-country" element={<RateCountry />} />
+          <Route path="/country/:countryName" element={<CountryPage />} />
         </Routes>
       </MainContent>
     </Router>
